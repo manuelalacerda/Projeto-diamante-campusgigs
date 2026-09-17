3b3598c3b79c385b479e121dd06b75c9a8edbfff
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-17 08:53:20 -0300

    Projeto iniciado

---
+++ campusgigs/src/main/java/br/fiap/com/campusgigs/CampusgigsApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.com.campusgigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusgigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusgigsApplication.class, args);
+	}
+
+}
---
+++ campusgigs/src/test/java/br/fiap/com/campusgigs/CampusgigsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.com.campusgigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusgigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
